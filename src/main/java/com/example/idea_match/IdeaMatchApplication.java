8a41ff29311d3f957df4545fc361838f079b125c
Krzysztof Kolodziej <krzysztof.kolodziej2212@gmail.com>
2025-05-08 20:43:23 +0200

    Init commit

---
+++ src/main/java/com/example/idea_match/IdeaMatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.idea_match;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaMatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/idea_match/IdeaMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.idea_match;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
