901cc904b3cfdff41f785cacd67a01c1cd89c8a5
Krzysztof Kolodziej <krzysztof.kolodziej2212@gmail.com>
2025-05-24 12:24:35 +0200

    Add configuration filterChain

---
+++ src/main/java/com/example/idea_match/user/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.idea_match.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/account/**").authenticated()
+                        .anyRequest().permitAll());
+
+        http.sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.formLogin(AbstractHttpConfigurer::disable);
+
+        http.cors(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
